2c21bd6a438d2344dc8e891d341348a1df7b1181
Genki Sugawara <sugawara@cookpad.com>
2015-11-02 09:59:11 +0900

    Fix spec

--- spec/faraday_middleware/aws_signers_v4_spec.rb
+++ spec/faraday_middleware/aws_signers_v4_spec.rb
@@ -17,18 +17,6 @@ describe FaradayMiddleware::AwsSignersV4 do
      "throttleSettings"=>{"burstLimit"=>1000, "rateLimit"=>500.0}}
   end
 
-  let(:expected_headers) do
-    {"User-Agent"=>"Faraday v0.9.1",
-     "X-Amz-Date"=>"20150101T000000Z",
-     "Host"=>"apigateway.us-east-1.amazonaws.com",
-     "X-Amz-Content-Sha256"=>
-      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
-     "Authorization"=>
-      "AWS4-HMAC-SHA256 Credential=akid/20150101/us-east-1/apigateway/aws4_request, " +
-      "SignedHeaders=host;user-agent;x-amz-content-sha256;x-amz-date, " +
-      "Signature=d25bb10ed5b6735974a3d1e0bae0bd8e4e28bddfd03a39e3e9ada780d54990a7"}
-  end
-
   let(:client) do
     faraday do |stub|
       stub.get('/account') do |env|
@@ -42,7 +30,39 @@ describe FaradayMiddleware::AwsSignersV4 do
     stub_const('Faraday::VERSION', '0.9.1')
   end
 
-  subject { client.get('/account').body }
+  context 'without query' do
+    let(:expected_headers) do
+      {"User-Agent"=>"Faraday v0.9.1",
+       "X-Amz-Date"=>"20150101T000000Z",
+       "Host"=>"apigateway.us-east-1.amazonaws.com",
+       "X-Amz-Content-Sha256"=>
+        "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+       "Authorization"=>
+        "AWS4-HMAC-SHA256 Credential=akid/20150101/us-east-1/apigateway/aws4_request, " +
+        "SignedHeaders=host;user-agent;x-amz-content-sha256;x-amz-date, " +
+        "Signature=d25bb10ed5b6735974a3d1e0bae0bd8e4e28bddfd03a39e3e9ada780d54990a7"}
+    end
 
-  it { is_expected.to eq response }
+    subject { client.get('/account').body }
+
+    it { is_expected.to eq response }
+  end
+
+  context 'with query' do
+    let(:expected_headers) do
+      {"User-Agent"=>"Faraday v0.9.1",
+       "X-Amz-Date"=>"20150101T000000Z",
+       "Host"=>"apigateway.us-east-1.amazonaws.com",
+       "X-Amz-Content-Sha256"=>
+        "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+       "Authorization"=>
+        "AWS4-HMAC-SHA256 Credential=akid/20150101/us-east-1/apigateway/aws4_request, " +
+        "SignedHeaders=host;user-agent;x-amz-content-sha256;x-amz-date, " +
+        "Signature=8287c520a389fbb0be36955e19d468d3c50d81cad922f59f2294a4c5b5cb6a73"}
+    end
+
+    subject { client.get('/account', foo: 'bar').body }
+
+    it { is_expected.to eq response }
+  end
 end
